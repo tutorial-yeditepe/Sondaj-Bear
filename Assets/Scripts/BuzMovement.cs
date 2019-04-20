c7df5157c81c40fd6988ce2d88cf78ffa9bf2ac4
Khorheal <aanilyildiz@gmail.com>
2019-04-20 16:34:51 +0300

    Finalized scaling game area boundary algorithm

--- Assets/Scripts/BuzMovement.cs
+++ Assets/Scripts/BuzMovement.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class BuzMovement : MonoBehaviour
 {
-    [SerializeField] [Range(1.0f, 10.0f)] public float moveSpeed = 5;
+    [SerializeField] [Range(1.0f, 10.0f)] public float moveSpeed = 5.0f;
+    private float width = Screen.width;
+    private float height = Screen.height;
+    private float charWidth;
     private Vector2 pos;
+    private Vector2 bottomCorner;
+    private Vector2 topCorner;
     private float center;
 
     private void Start()
     {
         pos = this.transform.position;
-        center = pos.x;
+        charWidth = this.GetComponent<SpriteRenderer>().bounds.size.x;
+        center = width / 2;
+        Debug.Log(charWidth);
+        Debug.Log(pos.x);
+        Debug.Log(width);
+        Debug.Log(height);
+        float camDistance = Vector2.Distance(transform.position, Camera.main.transform.position);
+        bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+        topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
+        Debug.Log(bottomCorner);
+        Debug.Log(topCorner);
     }
 
     void Update()
@@ -22,16 +37,15 @@ public class BuzMovement : MonoBehaviour
     void Move()
     {
         pos = this.transform.position;
-        //Touch touch = Input.GetTouch(0);
-        if (Input.GetMouseButton(0) && Input.mousePosition.y > 350)
+        if (Input.GetMouseButton(0) && Input.mousePosition.y > ((height*30)/100))
         { 
-            if (pos.x > 120.5)
+            if (pos.x > bottomCorner.x + charWidth/2)
                 if (Input.GetMouseButton(0) && Input.mousePosition.x < center - 0.5)
                 {
                     pos.x = pos.x - moveSpeed;
                     this.transform.position = pos;
                 }
-            if (pos.x < 790.5)
+            if (pos.x < topCorner.x - charWidth/2)
                 if (Input.GetMouseButton(0) && Input.mousePosition.x > center + 0.5)
                 {
                     pos.x = pos.x + moveSpeed;
