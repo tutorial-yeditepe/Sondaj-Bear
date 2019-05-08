734851e56830c2b4e3d8762217be6de828bd7ab1
yigitsimsek <yigidosimsek@gmail.com>
2019-05-08 12:44:06 +0300

    Fish Spawner and FishMovement scripts added

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+    private Vector3 pos;
+    private float fishSpeed = 5;
+    private int dir;
+
+    void Start()
+    {
+        string parentName = transform.parent.name;
+
+        if (parentName == "LeftSpawn")
+            dir = 1;
+        else
+            dir = -1;
+    }
+
+    void Update()
+    {
+        pos = transform.position;
+
+        pos.x += fishSpeed * dir;
+        transform.position = pos;
+
+        if (pos.x > 650 | pos.x < -400)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
