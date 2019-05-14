529c5df95d07ad721eca9f6b3f008f6b8c49bbb9
Khorheal <aanilyildiz@gmail.com>
2019-05-14 13:57:36 +0300

    Added collider and Hook mechanism

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -4,63 +4,75 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-    //flag to check if the user has tapped / clicked. 
-    //Set to true on click. Reset to false on reaching destination
     private bool flag = false;
-    //destination point
-    private Vector3 endPoint;
-    //alter this to change the speed of the movement of player / gameobject
-    public float duration = 50.0f;
-    //vertical position of the gameobject
-    private float yAxis;
+    private Vector2 endPoint;
+    public float duration = 1f;
+    public float speed = 4f;
+    private Vector2 startPoint;
+    RaycastHit2D hit;
+    Vector2 ray;
+    private float timeStartedLerping;
+    bool hasArrived = true;
 
     void Start()
     {
-        //save the y axis value of gameobject
-        yAxis = gameObject.transform.position.y;
+        startPoint = gameObject.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        hit = Physics2D.Raycast(ray, Vector2.zero);
 
         //check if the screen is touched / clicked   
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
         {
-            //declare a variable of RaycastHit struct
-            RaycastHit hit;
-            //Create a Ray on the tapped / clicked position
-            Ray ray;
-
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //Debug.Log(ray);
-
-            //Check if the ray hits any collider
-            if (Physics.Raycast(ray, out hit))
-            {
-                //set a flag to indicate to move the gameobject
-                flag = true;
-                //save the click / tap position
-                endPoint = hit.point;
-                //as we do not want to change the y axis value based on touch position, reset it to original y axis value
-                endPoint.y = yAxis;
-                Debug.Log(endPoint);
-            }
-
+            //if (hasArrived)
+                //Check if the ray hits any collider
+                if (hit.collider != null)
+                {
+                    Debug.Log("hit");
+                    //set a flag to indicate to move the gameobject
+                    flag = true;
+                    timeStartedLerping = Time.time;
+                    //save the click / tap position
+                    endPoint = hit.point;
+                    Debug.Log(endPoint);
+                }
         }
-        //check if the flag for movement is true and the current gameobject position is not same as the clicked / tapped position
-        if (flag && !Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude))
-        { //&& !(V3Equal(transform.position, endPoint))){
-          //move the gameobject to the desired position
-            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, endPoint, 1 / (duration * (Vector3.Distance(gameObject.transform.position, endPoint))));
-        }
-        //set the movement indicator flag to false if the endPoint and current gameobject position are equal
-        else if (flag && Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude))
-        {
-            flag = false;
-            Debug.Log("I am here");
-        }
-
+        //Debug.Log(hasArrived);
+        Debug.Log(flag);
     }
+
+    private void FixedUpdate()
+    {
+        if (flag)
+        {
+            hasArrived = false;
+            float timeSinceStarted = Time.time - timeStartedLerping;
+            float percentageComplete = timeSinceStarted / duration;
+            //Debug.Log(percentageComplete + "if");
+
+            gameObject.transform.position = Vector2.Lerp(gameObject.transform.position, endPoint, percentageComplete*speed);
+
+            if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.GetMouseButtonUp(0)))
+            {
+                flag = false;
+                timeStartedLerping = Time.time;
+            }
+        }
+        else
+        {
+            float timeSinceStarted = Time.time - timeStartedLerping;
+            float percentageComplete = timeSinceStarted / duration;
+            //Debug.Log(percentageComplete+"else");
+
+            gameObject.transform.position = Vector2.Lerp(gameObject.transform.position, startPoint, percentageComplete*speed);
+
+            /*if (percentageComplete >= 1.0f)
+                hasArrived = true;*/
+        }
+    }
+
 }
 
