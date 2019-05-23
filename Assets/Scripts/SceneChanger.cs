3b489f2f979d92c9ff7dfb5e3664ca89d4fde1a5
Khorheal <aanilyildiz@gmail.com>
2019-05-23 23:37:13 +0300

    Two splits merged and debugged

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     public void changeScene(string sceneName) {
 
-        Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
 
     }
     
