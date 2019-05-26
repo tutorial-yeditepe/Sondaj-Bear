6fcfa8d9743d15ed7233da17c46f18443f667354
Khorheal <aanilyildiz@gmail.com>
2019-05-26 22:50:10 +0300

    Changed spawner so that it can work with different phones

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -19,12 +19,12 @@ public class Spawner : MonoBehaviour
     {
         int prefab = Random.Range(0, spawnees.Length); // Index number for selecting a prefab.
         int side = Random.Range(0, spawnSides.Length); // Index number for selecting a spawn side (0 = left, 1 = right).
-        int spawnY = Random.Range(-110, 110); // A "y" value for our spawn position.
+        int spawnY = Random.Range(20, ((Screen.height * 50) / 100)); // A "y" value for our spawn position.
 
-        Vector3 spawnPos = spawnSides[side].transform.position;
+        Vector2 spawnPos = spawnSides[side].transform.localPosition;
         Quaternion spawnRot = spawnSides[side].transform.rotation;
 
-        spawnPos += new Vector3(0, spawnY, 0); // Changing the spawn position.
+        spawnPos += new Vector2(0, spawnY); // Changing the spawn position.
         
         Transform t = ((GameObject)Instantiate(spawnees[prefab], spawnPos, spawnRot, spawnSides[side].transform)).transform;
         //t.SetParent(spawnSides[side].transform, false);
