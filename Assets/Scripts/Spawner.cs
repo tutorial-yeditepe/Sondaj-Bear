ba2760631f69a878f1cf1fd9dc804387d8ba561d
yigitsimsek <yigidosimsek@gmail.com>
2019-05-29 05:35:58 +0300

    Spawn position fixed.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,23 +17,37 @@ public class Spawner : MonoBehaviour
 
     void SpawnObject()
     {
-        int prefab = Random.Range(0, spawnees.Length); // Index number for selecting a prefab.
+        int pIndex = Random.Range(0, spawnees.Length); // Index number for selecting a prefab.
         int side = Random.Range(0, spawnSides.Length); // Index number for selecting a spawn side (0 = left, 1 = right).
-        int spawnY = Random.Range(20, ((Screen.height * 45) / 100)); // A "y" value for our spawn position.
 
-        Vector2 spawnPos = spawnSides[side].transform.localPosition;
+        int min = (int)Camera.main.ViewportToWorldPoint(new Vector2(0f, 0.50f)).y;
+        int max = (int)Camera.main.ViewportToWorldPoint(new Vector2(0f, 0.019f)).y;
+
+        // int spawnY = Random.Range(20, ((Screen.height * 45) / 100)); // A "y" value for our spawn position.
+        int spawnY = Random.Range(min, max); // A "y" value for our spawn position.
+
+        Vector2 spawnPos = spawnSides[side].transform.position;
         Quaternion spawnRot = spawnSides[side].transform.rotation;
 
-        spawnPos += new Vector2(0, spawnY); // Changing the spawn position.
-        
-        Transform t = ((GameObject)Instantiate(spawnees[prefab], spawnPos, spawnRot, spawnSides[side].transform)).transform;
+        spawnPos = new Vector2(spawnPos.x, spawnY); // Changing the spawn position.
+
+        //Transform t = ((GameObject)Instantiate(spawnees[pIndex], spawnPos, spawnRot, spawnSides[side].transform)).transform;
         //t.SetParent(spawnSides[side].transform, false);
 
-        if (side == 0 && spawnees[prefab].tag != "PowerUp")
+        GameObject obj = Instantiate(spawnees[pIndex], spawnPos, spawnRot, spawnSides[side].transform);
+
+        /*if (side == 0 && spawnees[pIndex].tag != "PowerUp")
         {
             Vector3 reverse = t.localScale;
             reverse.x = reverse.x * -1;
             t.localScale = reverse;
+        }*/
+
+        if (side == 0 && spawnees[pIndex].tag != "PowerUp")
+        {
+            Vector3 reverse = obj.transform.localScale;
+            reverse.x = reverse.x * -1;
+            obj.transform.localScale = reverse;
         }
     }
 }
